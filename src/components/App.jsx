432139c0d4a895b018c8766fbd49074020f567e0
Dmitriy Bondarenko <dabwork1982@gmail.com>
2022-03-31 21:09:18 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+// import { Button } from './button/button';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Вiдмова вiд 1С
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        Вiдмова вiд 1С
+      </div>
+    </>
   );
 };
