9bb71737f7686c5f86f1cb897ab694d4a74e7cb6
Nazimkov-1984 <nazimkoff@gmail.com>
2022-04-01 19:51:01 +0300

    форма

--- src/components/Radio/RadioButton.jsx
+++ src/components/Radio/RadioButton.jsx
@@ -1,17 +1,23 @@
-import "./RadioButton.css";
+import './RadioButton.css';
 import { useCallback, useState } from 'react';
 
-const RadioButton = (props)=> {
+const RadioButton = props => {
   const [isCheked, setIsCheked] = useState(props.isChecked);
-  const onClickHandler = useCallback(()=> {
-  setIsCheked(prevState => !prevState);
-  },[])
+  const onClickHandler = useCallback(() => {
+    setIsCheked(prevState => !prevState);
+  }, []);
 
-  return(
-    <div className="radioButton_wrapper"  onClick={onClickHandler}>
-    <input name={props.name} className="radioButton"  id = {props.name} type="radio" checked={isCheked} />
+  return (
+    <div className="radioButton_wrapper" onClick={onClickHandler}>
+      <input
+        name={props.name}
+        className="radioButton"
+        id={props.name}
+        type="radio"
+        checked={isCheked}
+      />
       <label className="radio_label">{props.labelText}</label>
     </div>
-  )
-}
+  );
+};
 export default RadioButton;
