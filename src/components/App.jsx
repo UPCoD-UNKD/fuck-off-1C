bb710e916ec4a733b367a5b06d455c12a3b808e8
Nazimkov-1984 <nazimkoff@gmail.com>
2022-06-01 15:38:58 +0300

    refactor modal with mobX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,17 @@ import FormOrganization from './form_organization/FormOrganization';
 import { Footer } from './footer/Footer';
 import './App.css';
 import Modal from './Modal/Modal';
-import { useState } from 'react';
 
 export const App = () => {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  const [isCorrectSendData, setIsCorrectSendData] = useState(true);
   
   return (
     <div className="app">
       <Hero />
       <Mission />
       <FormOrganization
-        openModal={setIsOpenModal}
-        setIsCorrectSendData={setIsCorrectSendData}
       />
       <Footer />
       <Modal
-        isCorrectSendData={isCorrectSendData}
-        isOpenModal={isOpenModal}
-        closeModal={setIsOpenModal}
       />
     </div>
   );
---
+++ src/constants/additionalInputData.js
@@ -0,0 +1,24 @@
+const ADDITIONAL_INPUT_DATA = [
+  {
+    id: 'concrete',
+    type: 'text',
+    name: 'Що саме?',
+    placeholder: 'Якщо інше, то що саме?',
+    required: true,
+    value: '',
+    isRequired: false,
+    isHidePlaceholder: false,
+  },
+  {
+    id: 'condition',
+    type: 'text',
+    name: 'Умови відмови',
+    placeholder: 'Умови відмови',
+    required: true,
+    value: '',
+    isRequired: false,
+    isHidePlaceholder: false,
+  },
+];
+
+export default ADDITIONAL_INPUT_DATA;
